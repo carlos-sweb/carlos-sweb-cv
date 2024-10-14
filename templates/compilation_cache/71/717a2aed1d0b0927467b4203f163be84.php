1fe87132ec4ee625f2666df02ecad15355112eaf
carlos-sweb <c4rl0sill3sc4@gmail.com>
2024-10-14 20:23:38 -0300

    add space bottom

--- templates/compilation_cache/71/717a2aed1d0b0927467b4203f163be84.php
+++ templates/compilation_cache/71/717a2aed1d0b0927467b4203f163be84.php
@@ -40,23 +40,20 @@ class __TwigTemplate_7fa0604bf68d0adbc52957daa153622c extends Template
         $macros = $this->macros;
         // line 1
         yield "<div class=\"row\" style=\"height:100%!important;min-height:100%\" >
-
 \t<div class=\"col-xs-12 col-sm-12 col-md-6 col-lg-3 center-xs mdc-elevation--z2 animated fadeIn\" style=\"padding-bottom:56px;  background-color: #ECEFF1;\" >
 \t\t";
+        // line 3
+        yield from         $this->loadTemplate("parts/slide-left.html.twig", "content.html.twig", 3)->unwrap()->yield($context);
         // line 4
-        yield from         $this->loadTemplate("parts/slide-left.html.twig", "content.html.twig", 4)->unwrap()->yield($context);
-        // line 5
         yield "\t</div>
-
 \t<div class=\"col-xs-12 col-sm-12 col-md-6 col-lg-9 start-xs animated fadeIn\" style=\"background-color:#fafafa\">
 \t\t";
-        // line 8
-        yield from         $this->loadTemplate("parts/slide-right.html.twig", "content.html.twig", 8)->unwrap()->yield($context);
-        // line 9
-        yield "\t</div>
-
-</div>
-";
+        // line 6
+        yield from         $this->loadTemplate("parts/slide-right.html.twig", "content.html.twig", 6)->unwrap()->yield($context);
+        yield "\t\t
+\t</div>
+\t<div class=\"col-xs-12 col-sm-12 col-md-6 col-lg-3 start-xs\" style=\"height:150px;\"></div>
+</div>";
         yield from [];
     }
 
@@ -81,22 +78,19 @@ class __TwigTemplate_7fa0604bf68d0adbc52957daa153622c extends Template
      */
     public function getDebugInfo(): array
     {
-        return array (  56 => 9,  54 => 8,  49 => 5,  47 => 4,  42 => 1,);
+        return array (  52 => 6,  48 => 4,  46 => 3,  42 => 1,);
     }
 
     public function getSourceContext(): Source
     {
         return new Source("<div class=\"row\" style=\"height:100%!important;min-height:100%\" >
-
 \t<div class=\"col-xs-12 col-sm-12 col-md-6 col-lg-3 center-xs mdc-elevation--z2 animated fadeIn\" style=\"padding-bottom:56px;  background-color: #ECEFF1;\" >
 \t\t{% include('parts/slide-left.html.twig') %}
 \t</div>
-
 \t<div class=\"col-xs-12 col-sm-12 col-md-6 col-lg-9 start-xs animated fadeIn\" style=\"background-color:#fafafa\">
-\t\t{% include('parts/slide-right.html.twig') %}
+\t\t{% include('parts/slide-right.html.twig') %}\t\t
 \t</div>
-
-</div>
-", "content.html.twig", "/home/sweb/github-project/carlos-sweb-cv/templates/content.html.twig");
+\t<div class=\"col-xs-12 col-sm-12 col-md-6 col-lg-3 start-xs\" style=\"height:150px;\"></div>
+</div>", "content.html.twig", "/home/sweb/github-project/carlos-sweb-cv/templates/content.html.twig");
     }
 }
