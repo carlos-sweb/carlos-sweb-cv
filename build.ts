43a2236f31151672b40597345ec6764b7a9bd049
carlos-sweb <c4rl0sill3sc4@gmail.com>
2025-12-27 01:54:44 -0300

    Change to php to ts with zig-pug

--- build.ts
+++ build.ts
@@ -1,19 +1,28 @@
 import { PugCompiler } from 'zig-pug'
 import { YAML } from "bun"
 
-const links = YAML.parse(await Bun.file("yml/links.yml").text())
-const profile = YAML.parse(await Bun.file("yml/profile.yml").text())
-const titles = YAML.parse(await Bun.file("yml/en/titles.yml").text())
-const skills = YAML.parse(await Bun.file("yml/skills.yml").text())
+const links = YAML.parse(await Bun.file("yml/links.yml").text()),
+profile     = YAML.parse(await Bun.file("yml/profile.yml").text()),
+skills      = YAML.parse(await Bun.file("yml/skills.yml").text()),
+
+projects    = YAML.parse(await Bun.file("yml/en/projects.yml").text()),
+titles      = YAML.parse(await Bun.file("yml/en/titles.yml").text()),
+employments = YAML.parse(await Bun.file("yml/en/employments.yml").text()),
+education = YAML.parse(await Bun.file("yml/en/education.yml").text())
+
+    
 
 // Using PugCompiler class
 const compiler: PugCompiler = new PugCompiler();
 compiler
+    .setString("lang","en") 
     .setArray("links",links)
     .setObject('profile', profile)
     .setObject('titles', titles)
-    .setArray('skills', skills)
+    .setArray('skills',skills)
+    .setArray('projects',projects)
 
 const result: string = compiler.compile( await Bun.file("templates/home.zpug").text() );
 
 console.log(result)
+
