4952e7cf7254e3664c17de4c59c04981ecae7c22
carlos-sweb <c4rl0sill3sc4@protonmail.com>
2026-04-10 23:53:51 -0400

    project move to bun and ts used zig-pug template

--- build.ts
+++ build.ts
@@ -3,28 +3,25 @@ import { YAML } from "bun"
 
 const links = YAML.parse(await Bun.file("yml/links.yml").text()),
 profile     = YAML.parse(await Bun.file("yml/profile.yml").text()),
-skills      = YAML.parse(await Bun.file("yml/skills.yml").text()),
+skills      = YAML.parse(await Bun.file("yml/skills.yml").text())
 
-projects    = YAML.parse(await Bun.file("yml/en/projects.yml").text()),
-titles      = YAML.parse(await Bun.file("yml/en/titles.yml").text()),
-employments = YAML.parse(await Bun.file("yml/en/employments.yml").text()),
-education = YAML.parse(await Bun.file("yml/en/education.yml").text())
+for(const lang of ["en","es"]){
 
+    const projects = YAML.parse(await Bun.file("yml/"+lang+"/projects.yml").text()),
+    titles      = YAML.parse(await Bun.file("yml/"+lang+"/titles.yml").text()),
+    employments = YAML.parse(await Bun.file("yml/"+lang+"/employments.yml").text()),
+    education = YAML.parse(await Bun.file("yml/"+lang+"/education.yml").text()),
+    compiler: PugCompiler = new PugCompiler({ pretty:false });
 
-
-const compiler: PugCompiler = new PugCompiler({ pretty:true });
-compiler
-    .setString("lang","en") 
+    compiler
+    .setString("lang",lang) 
     .setArray("links",links)
     .setObject('profile', profile)
     .setObject('titles', titles)
     .setArray('skills',skills)
     .setArray('projects',projects)
 
+    const result: string = compiler.compile( await Bun.file("templates/home.zpug").text() );        
+    await Bun.write("public/"+(lang == "en" ? "index.html" : "es.html"),result)
 
-
-const result: string = compiler.compile( await Bun.file("templates/home.zpug").text() );
-
-
-await Bun.write("public/zpug.html",result)
-
+}
